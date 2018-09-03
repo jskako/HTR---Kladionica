c4676a2086bd24be81409ae2878dee793dfde7aa
Josip S <35076900+jskako@users.noreply.github.com>
2018-09-03 21:30:38 +0200

    Add files via upload

--- SQL Scripts/10_Generiranje_Parova.sql
+++ SQL Scripts/10_Generiranje_Parova.sql
@@ -1,5 +1,21 @@
 USE [HTR]
 
+DECLARE @myDate date
+DECLARE @isEmpty date
+set @myDate = GETDATE()
+set @isEmpty = ( SELECT TOP 1 F07DTI FROM parovi where F07DTI = @myDate ORDER BY F07DTI DESC)
+SELECT LEFT(CONVERT(VARCHAR, @isEmpty, 120), 10)
+
+if @isEmpty is null
+begin
+
+DECLARE @TopRow int
+set @TopRow = (SELECT TOP 1 F07IDP FROM parovi ORDER BY F07IDP DESC)
+if @TopRow is null
+set @TopRow = 1
+set @TopRow = @TopRow+1
+Declare @TopRowTop int
+set @TopRowTop = @TopRow + 100
 Declare @RandomTeamOneInt int
 Declare @RandomTeamTwoInt int
 Declare @RandomTeamOne varchar(200)
@@ -11,12 +27,11 @@ Declare @KoefOne decimal (5,2)
 Declare @KoefTwo decimal (5,2)
 Declare @KoefTie decimal (5,2)
 Declare @MyTip int
-Declare @Id int
 Declare @DateStart	Date =  CONVERT(date,GETDATE())
 		,@DateEnd	Date = (SELECT DATEADD(day, 7, CONVERT(date,GETDATE())) AS DateAdd)
-Set @Id = 1
 
-While @Id <= 100
+
+While @TopRow <= @TopRowTop
 Begin 
    DoesPairExist: 
    Set @RandomTeamOneInt = (SELECT TOP 1 F05EID FROM Ekipe ORDER BY NEWID())
@@ -29,7 +44,7 @@ Begin
    GOTO IsTeamSame
    Declare @Counter int 
    Set @Counter = 1
-   WHILE(@Counter < @Id)
+   WHILE(@Counter < @TopRow)
    BEGIN
    IF(@RandomTeamOne = (Select F07TM1 from Parovi where F07IDP = @Counter) AND @RandomTeamTwo = (Select F07TM2 from Parovi where F07IDP = @Counter))
    GOTO DoesPairExist
@@ -60,7 +75,13 @@ Begin
 
    SET @Time = (SELECT (convert(Time, DateAdd(second, @randomSeconds, @startTime)))  AS RandomTime)
    Insert Into Parovi (F07IDP, F07TM1, F07TM2, F07KO1, F07KO2, F07KOX, F07DTI, F07VRI, F07SPO)
-   values (@Id,@RandomTeamOne,@RandomTeamTwo,@KOEFONE,@KOEFTWO,@KoefTie, (Select DateAdd(Day, Rand() * DateDiff(Day, @DateStart, @DateEnd), @DateStart)),@Time, @MyTip)
+   values (@TopRow,@RandomTeamOne,@RandomTeamTwo,@KOEFONE,@KOEFTWO,@KoefTie, (Select DateAdd(Day, Rand() * DateDiff(Day, @DateStart, @DateEnd), @DateStart)),@Time, @MyTip)
 
-   Set @Id = @Id + 1
+   Set @TopRow = @TopRow + 1
 End
+
+end
+
+--delete from parovi
+
+--select * from parovi
