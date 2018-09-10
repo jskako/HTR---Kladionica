554c4e7aa2bb18f6dd99f60c7023b92dcc0cea77
jskako <jskako@fesb.hr>
2018-09-10 20:27:53 +0200

    Punjenje tablica ostalih sportova

--- src/htr/UserView.java
+++ src/htr/UserView.java
@@ -8,9 +8,9 @@ package htr;
 import java.awt.Frame;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.swing.table.DefaultTableModel;
 import net.proteanit.sql.DbUtils;
 
 /**
@@ -92,8 +92,22 @@ public class UserView extends javax.swing.JFrame {
 
     private void PunjenjeTablicePodacima(String Time) {
         try {
+            //Nogomet
             RS = CALIzb.main(Conn, "select F07TM1 Domacin, F07TM2 Protivnik, F07KO1 '1', F07KOX X, F07KO2 '2', REPLACE( F07VRI, RIGHT(F07VRI, 11), '' ) Vrijeme from parovi where F07SPO = '1' and F07DTI = '" + Time + "' ORDER BY F07VRI ASC");
             tableNogomet.setModel(DbUtils.resultSetToTableModel(RS));
+
+            //Kosarka
+            RS = CALIzb.main(Conn, "select F07TM1 Domacin, F07TM2 Protivnik, F07KO1 '1', F07KOX X, F07KO2 '2', REPLACE( F07VRI, RIGHT(F07VRI, 11), '' ) Vrijeme from parovi where F07SPO = '2' and F07DTI = '" + Time + "' ORDER BY F07VRI ASC");
+            tableKosarka.setModel(DbUtils.resultSetToTableModel(RS));
+
+            //Tenis
+            RS = CALIzb.main(Conn, "select F07TM1 Domacin, F07TM2 Protivnik, F07KO1 '1', F07KOX X, F07KO2 '2', REPLACE( F07VRI, RIGHT(F07VRI, 11), '' ) Vrijeme from parovi where F07SPO = '4' and F07DTI = '" + Time + "' ORDER BY F07VRI ASC");
+            tableHokej.setModel(DbUtils.resultSetToTableModel(RS));
+
+            //Hokej
+            RS = CALIzb.main(Conn, "select F07TM1 Domacin, F07TM2 Protivnik, F07KO1 '1', F07KOX X, F07KO2 '2', REPLACE( F07VRI, RIGHT(F07VRI, 11), '' ) Vrijeme from parovi where F07SPO = '3' and F07DTI = '" + Time + "' ORDER BY F07VRI ASC");
+            tableTenis.setModel(DbUtils.resultSetToTableModel(RS));
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -113,7 +127,7 @@ public class UserView extends javax.swing.JFrame {
         lbl_UserName = new javax.swing.JLabel();
         lbl_Balance = new javax.swing.JLabel();
         pickDate = new com.toedter.calendar.JDateChooser();
-        jTextField1 = new javax.swing.JTextField();
+        btnPotvrdi = new javax.swing.JButton();
         MySoccerPanel = new javax.swing.JPanel();
         img_Soccer = new javax.swing.JLabel();
         lbl_Nogomet1 = new javax.swing.JLabel();
@@ -157,53 +171,62 @@ public class UserView extends javax.swing.JFrame {
 
         pickDate.setDateFormatString("yyyy-MM-dd");
         pickDate.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        pickDate.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                pickDateFocusLost(evt);
+            }
+        });
         pickDate.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
             public void propertyChange(java.beans.PropertyChangeEvent evt) {
                 pickDatePropertyChange(evt);
             }
         });
+        pickDate.addVetoableChangeListener(new java.beans.VetoableChangeListener() {
+            public void vetoableChange(java.beans.PropertyChangeEvent evt)throws java.beans.PropertyVetoException {
+                pickDateVetoableChange(evt);
+            }
+        });
 
-        jTextField1.setText("jTextField1");
+        btnPotvrdi.setFont(new java.awt.Font("Segoe UI", 2, 14)); // NOI18N
+        btnPotvrdi.setText("Potvrdi");
+        btnPotvrdi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPotvrdiActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(748, Short.MAX_VALUE)
-                        .addComponent(lbl_UserName)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(lbl_Balance)
-                        .addGap(621, 621, 621))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(523, 523, 523)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(759, Short.MAX_VALUE)
+                .addComponent(lbl_UserName)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lbl_Balance)
+                .addGap(621, 621, 621)
                 .addComponent(img_Logo)
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(pickDate, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnPotvrdi, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                .addComponent(lbl_UserName)
-                                .addComponent(lbl_Balance))
-                            .addComponent(img_Logo)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(72, 72, 72)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
-                .addComponent(pickDate, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(lbl_UserName)
+                        .addComponent(lbl_Balance))
+                    .addComponent(img_Logo))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnPotvrdi, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(pickDate, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -514,6 +537,32 @@ public class UserView extends javax.swing.JFrame {
 
     }//GEN-LAST:event_pickDatePropertyChange
 
+    private void pickDateFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_pickDateFocusLost
+        // TODO add your handling code here:
+        tableNogomet.setModel(new DefaultTableModel());
+        System.out.println(sd);
+        System.out.println(sd);
+        System.out.println(sd);
+        System.out.println(sd);
+        sd = dateFormat.format(pickDate.getDate());
+        PunjenjeTablicePodacima(sd);
+    }//GEN-LAST:event_pickDateFocusLost
+
+    private void btnPotvrdiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPotvrdiActionPerformed
+        // TODO add your handling code here:
+        tableNogomet.setModel(new DefaultTableModel());
+        System.out.println(sd);
+        System.out.println(sd);
+        System.out.println(sd);
+        System.out.println(sd);
+        sd = dateFormat.format(pickDate.getDate());
+        PunjenjeTablicePodacima(sd);
+    }//GEN-LAST:event_btnPotvrdiActionPerformed
+
+    private void pickDateVetoableChange(java.beans.PropertyChangeEvent evt)throws java.beans.PropertyVetoException {//GEN-FIRST:event_pickDateVetoableChange
+        // TODO add your handling code here:
+    }//GEN-LAST:event_pickDateVetoableChange
+
     /**
      * @param args the command line arguments
      */
@@ -523,6 +572,7 @@ public class UserView extends javax.swing.JFrame {
     private javax.swing.JPanel MyHockeyPanel;
     private javax.swing.JPanel MySoccerPanel;
     private javax.swing.JPanel MyTennisPanel;
+    private javax.swing.JButton btnPotvrdi;
     private javax.swing.JLabel imgHockey;
     private javax.swing.JLabel imgTennis;
     private javax.swing.JLabel img_Logo;
@@ -535,7 +585,6 @@ public class UserView extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel lbl_Balance;
     private javax.swing.JLabel lbl_Hokej;
     private javax.swing.JLabel lbl_Kosarka;
